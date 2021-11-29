52c51b7e4f2f30e9c57ba117fc6007ab1553ce63
James Marsh <marshrjames@gmail.com>
2021-11-29 11:31:16 -0700

    addex port env variable

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = 5038;
+const port = process.end.PORT || 5038;
 
 app.use(cors());
 app.use(express.json());
