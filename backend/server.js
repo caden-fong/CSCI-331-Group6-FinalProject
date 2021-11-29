af0a375e6e8b764fb486bee0433616d368e7d732
robert.marsh1 <t97h748@csci331.cs.montana.edu>
2021-11-29 11:35:58 -0700

    fixed typo in server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.end.PORT || 5038;
+const port = process.env.PORT || 5038;
 
 app.use(cors());
 app.use(express.json());
