2c7b6c221a7bf118f6a900edd243afb242ec9b01
James Marsh <marshrjames@gmail.com>
2021-11-29 14:30:08 -0700

    changed ports in login page

--- src/components/more.component.js
+++ src/components/more.component.js
@@ -4,14 +4,31 @@ import './../App.css'
 export default class More extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      id: 'Not logged in.'
+    }
+
+    if (this.props.user._id) {
+      this.state.id = this.props.user._id;
+    }
 
   }
+  
+  
 
     render() {
     return (
       <div className= "container-fluid">
         <p>{this.props.user._id}</p>
+      
+
+      <div className = "container-fluid logpg">
+      <h3>User:</h3>
+      <h4>{this.state.id}</h4>
       </div>
+      <div className="spacing">.</div>
+      </div>
+
     )
   }
 }
