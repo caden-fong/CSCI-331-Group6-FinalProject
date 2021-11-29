a26400e12def390daa72f4cf4704f882614f4b66
James Marsh <marshrjames@gmail.com>
2021-11-29 15:22:00 -0700

    added character route

---
+++ backend/routes/characters.js
@@ -0,0 +1,38 @@
+
+const router = require('express').Router();
+let Character = require('../models/character.model');
+
+router.route('/:id').get((req, res) => {
+    Character.findById(req.params.id)
+      // .then(character => res.json(character))
+      .then(console.log(character))
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+
+router.route('/').get((req, res) => {
+  Character.find()
+      .then(characters => res.json(characters))
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+
+router.route('/:id').delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+      .then(() => res.json('Character deleted.'))
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+  
+router.route('/update/:id').post((req, res) => {
+    Exercise.findById(req.params.id)
+      .then(character => {
+        character.name = req.body.name;
+        character.level = Number(req.body.level);
+        character.race = req.body.race;
+  
+        character.save()
+          .then(() => res.json('Character updated!'))
+          .catch(err => res.status(400).json('Error: ' + err));
+      })
+      .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
