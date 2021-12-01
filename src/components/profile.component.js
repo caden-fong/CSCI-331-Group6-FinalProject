5ffe5f07c2f45563f1fa8eaa76afb20955afa194
KayleeFong <77719343+KayleeFong@users.noreply.github.com>
2021-11-30 17:57:09 -0700

    fixing up logged in views

--- src/components/profile.component.js
+++ src/components/profile.component.js
@@ -5,15 +5,43 @@ import './../App.css'
 export default class Profile extends Component {
   constructor(props) {
     super(props);
-
   }
 
     render() {
     return (
       <>
-        <div className= "container-fluid">
+        <div className= "container-fluid userinfo">
           <img src={profilepic} className= "profilelogo" alt="Character Creator Logo"/>
           <h3 className="userp">dsfsdfdsfdsfsde</h3>
+        </div>
+
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col- prf">
+              <div className="card indchar">
+                <div className="card-body">
+                  <h5 className="card-title">Player's Handbook</h5>
+                  <a href="#" class="btn btn-dark stretched-link">View</a>
+                </div>
+              </div>
+            </div>
+            <div className="col- prf">
+              <div className="card indchar">
+                <div className="card-body">
+                  <h5 className="card-title">Player's Handbook</h5>
+                  <a href="#" class="btn btn-dark stretched-link">View</a>
+                </div>
+              </div>
+            </div>
+            <div className="col- prf">
+              <div className="card indchar">
+                <div className="card-body">
+                  <h5 className="card-title">Player's Handbook</h5>
+                  <a href="#" class="btn btn-dark stretched-link">View</a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </>
     )
