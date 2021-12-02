cea619f6773abc8c5cb3702f318e5eedc6635b1e
Caden Fong <caden-fong@users.noreply.github.com>
2021-12-01 20:18:43 -0700

    First attempt at viewer information query

--- src/components/viewer.component.js
+++ src/components/viewer.component.js
@@ -3,51 +3,41 @@ import CCLogo from './../CCLogo.svg';
 import p1 from './../p1.jpg';
 import './../App.css'
 import axios from 'axios';
-import { useNavigate } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
 
-const Character = props => (
-  <tr>
-    <td>{props.character.name}</td>
-    <td>{props.character.level}</td>
-    <td>{props.character.race}</td>
-    <td>{props.character.class}</td>
-  </tr>
-)
 
 class Viewer extends Component {
   constructor(props) {
     super(props);
-    this.state = {characters: []}
 
-
+    this.state = {character: {}};
   }
 
   componentDidMount() {
-    axios.get('/characters/')
+    axios.get('/characters/' + this.props.params.id)
         .then(response => {
             this.setState({ characters: response.data });
-            console.log(this.state.characters);
+            console.log(this.state.character);
         })
         .catch((error) => {
             console.log(error);
-        })
+        });
   }
-
-  characterList() {
-    return this.state.characters.map(currentcharacter => {
-      return <Character character={currentcharacter}/>;
-    })
-  }
-
 
     render() {
     return (
-      <div></div>
+      <div>{this.state.character.name}
+      {this.state.character.level}
+      {this.state.character.skills}
+      {this.state.character.race}
+      </div>
+
     )
   }
 }
 
 export default (props) => {
+  const params = useParams();
   const navigate = useNavigate();
-  return (<Viewer {...props} navigate={navigate}/>);
+  return (<Viewer {...props} navigate={navigate} params={params}/>);
 }
